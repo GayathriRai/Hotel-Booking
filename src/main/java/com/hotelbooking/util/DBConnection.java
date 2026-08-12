d0778cb8ef4664c6d76de47cd74cf41ae4199923
Gayathri Rai <117651493+GayathriRai@users.noreply.github.com>
2026-08-12 11:35:52 +0530

    Update DBConnection.java

--- src/main/java/com/hotelbooking/util/DBConnection.java
+++ src/main/java/com/hotelbooking/util/DBConnection.java
@@ -5,9 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/hotel_booking_db?useSSL=false&serverTimezone=Asia/Kolkata&allowPublicKeyRetrieval=true";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+
+    private static final String URL =
+        "jdbc:mysql://localhost:3306/hotel_booking_db?useSSL=false&serverTimezone=Asia/Kolkata&allowPublicKeyRetrieval=true";
+
+    private static final String USER = "hotelapp";
+    private static final String PASSWORD = "YOUR_HOTELAPP_PASSWORD";
 
     private DBConnection() {}
 
